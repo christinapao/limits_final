9b9d6f7228f280fe965bf8ffdd7719852708a69a
Nick Kaashoek <nick.kaashoek@gmail.com>
2024-05-06 14:29:23 -0400

    Fixed join script to work on global data
    - joined_data has the result of the join, should look like the idb data
      with extra columns. Scar countries NOT REMOVED
    - data should be normalized on 0-1 scale (min-max normalization)
    - codes.csv has the mapping from 3 digit codes to 2 digit codes used in
      the joining process

--- join.py
+++ join.py
@@ -1,27 +1,25 @@
 import argparse
 import pandas as pd
+import numpy as np
 
-to_geo = lambda x : f"W140000WO{x.upper()}"
 
-IDP_COLS = ["#YR", "POP", "NMR", "DEATHS", "BIRTHS"]
+SCAR_COUNTRIES = ["RU", "KH", "RW"]
 
-def join_idb(df, idb, code):
-    country_idb = idb[idb['GEO_ID'] == to_geo(code)]
-    latest_year = df.index.max()
-    earliest_year = df.index.min()
-    # Filter out the years so that we match on both dataframes
-    country_idb = country_idb[(country_idb['#YR'] <= latest_year) & (country_idb['#YR'] >= earliest_year)]
-    country_idb = country_idb[IDP_COLS]
-    country_idb.set_index("#YR", inplace=True)
-    # Join on the year column
-    print("BEFORE MERGE", df)
-    merged = df.reset_index().merge(country_idb, how='outer', left_on="Year", right_on="#YR").set_index("Year")
-    new_cols = {}
-    for col in merged:
-        if type(col) == tuple:
-            new_cols[col] = col[1]
-    merged.rename(columns=new_cols, inplace=True)
-    return merged
+col_to_code = lambda col : col.split("_")[0]
+
+
+def join_idb(df, idb):
+    """Join the idb data with the world bank data"""
+    min_year = df["Year"].min()
+    max_year = df["Year"].max()
+    idb_df = idb[(idb["#YR"] >= min_year) & (idb["#YR"] <= max_year)]
+    # Now we should just be able to join on (year, code) pairing
+    print("World bank shape:", df.shape)
+    merged_df = df.reset_index().merge(idb_df, left_on=["Year", "Code"], right_on=["#YR", "GEO_ID"], how="inner").set_index(["Country", "Year", "Code"])
+    merged_df.drop(columns=["#YR", "GEO_ID"], inplace=True)
+    # We lose some rows in the merge, but that's probably fine. Mostly likely due to mismatch in country codes
+    print("Merged shape:", merged_df.shape)
+    return merged_df
 
 def filter_country_df(df):
     col_orders = []
@@ -36,15 +34,45 @@ def filter_country_df(df):
     # print(df)
     return df
 
-def fix_country_df(df):
-    new_cols = {}
-    for idx, col in enumerate(df.columns):
-        series_name = df[col].iloc[0]
-        new_cols[col] = (col, series_name)
-    df.rename(columns=new_cols, inplace=True)
-    df = df.iloc[1:].copy()
-    df.index.name = "Year"
-    df.index = df.index.astype(int)
+def fix_worldbank_df(df, codes):
+    """Fix the world bank data to use iso2 codes"""
+    # Just construct a dictionary because it's faster
+    three_to_two = dict()
+    for _, row in codes.iterrows():
+        three_to_two[row["alpha-3"]] = row["alpha-2"]
+    # Remove everything that's not in the dictionary because finding a match is futile
+    df = df[df["Code"].isin(three_to_two)].copy()
+    # Remove columns with high nan percentage
+    drop_cols = []
+    for col in df.columns:
+        na_count = df[col].isna().sum()
+        if na_count >= len(df) * 0.9:
+            drop_cols.append(col)
+    df.drop(columns=drop_cols, inplace=True)
+    # Swap all the codes to the format used by the idb data
+    df["Code"] = df["Code"].apply(lambda x : f"W140000WO{three_to_two[x].upper()}")
+    return df
+
+
+def normalize_col(col, df: pd.DataFrame):
+    col_max, col_min = df[col].max(), df[col].min()
+    code = col.split("_")[0]
+    def normalize_value(x):
+        if not pd.notna(x):
+            return x
+        return (x-col_min)/(col_max-col_min)
+
+    if col_max == col_min:
+        # If all the values are the same, set to 0
+        df[col] = df[col].apply(lambda x: 0)
+    else:
+        df[col] =  df[col].apply(normalize_value)
+
+def normalize_output(df):
+    """take the joined dataframe (idb + world bank) and normalize the columns"""
+    # All the remaining columns should be numbers
+    for col in df.columns:
+        normalize_col(col, df)
     return df
 
 if __name__ == "__main__":
@@ -53,13 +81,23 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help="input file")
     parser.add_argument('--idb', help="idb file")
-    parser.add_argument('--code', help="country code to use")
     args = parser.parse_args()
 
-    df = pd.read_csv(args.input, index_col=0)
-    df = fix_country_df(df)
-    df = filter_country_df(df)
-    idb_df = pd.read_csv(args.idb, delimiter='|')
-    joined_data = join_idb(df, idb_df, args.code)
-    print(joined_data) 
-    joined_data.to_csv(f"joined_data/{args.code}_joined.csv", index=True)
+    # Don't filter nans because Namibia's two letter code is NA...
+    codes_df = pd.read_csv("codes.csv", na_filter=False)
+    print(codes_df)
+
+    df = pd.read_csv(args.input, index_col=0, na_values=[".."])
+    df = fix_worldbank_df(df, codes_df)
+
+    idb_df = None
+    if args.idb.endswith("txt"):
+        idb_df = pd.read_csv(args.idb, delimiter='|')
+        idb_df.to_parquet(args.idb.replace("txt", "parquet"))
+    # Parquet files are a bit faster, let's prefer them
+    if args.idb.endswith("parquet"):
+        idb_df = pd.read_parquet(args.idb)
+    merged_df = join_idb(df, idb_df)
+    normalize_output(merged_df)
+    print(merged_df)
+    merged_df.to_csv("joined_data.csv", index=True, index_label=["Country", "Year", "Code"])
